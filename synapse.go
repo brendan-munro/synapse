985d8c03300ffb78a5e70044cdc370c97f9b25df
Kris Runzer <kris.runzer@gmail.com>
2016-02-04 09:07:03 -0800

    Further extend usability - Add Handler interface; used in place of HandlerFunc for chaining - Make Chain immuteable by hiding constructors behind a struct - Add documentation

--- synapse.go
+++ synapse.go
@@ -6,46 +6,95 @@ import (
 	"golang.org/x/net/context"
 )
 
-type HandlerFunc func(c context.Context, w http.ResponseWriter, r *http.Request)
+// HandlerFunc type is an adapter to allow the use of ordinary functions as
+// handlers.  If fn is a function with the appropriate signature, HandlerFunc(f)
+// is a Handler object that calls fn
+type HandlerFunc func(context.Context, http.ResponseWriter, *http.Request)
 
+// ServeHTTP calls fn.ServeHTTPC(context.Background(), w, r)
 func (fn HandlerFunc) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fn(context.Background(), w, r)
+	fn.ServeHTTPC(context.Background(), w, r)
 }
 
-type Constructor func(next HandlerFunc) HandlerFunc
+// ServeHTTPC calls fn(c, w, r)
+func (fn HandlerFunc) ServeHTTPC(c context.Context, w http.ResponseWriter, r *http.Request) {
+	fn(c, w, r)
+}
 
-type Chain []Constructor
+// Handler interface can be implemented on a object to serve a particular path or
+// subtree in the HTTP server.
+//
+// ServerHTTPC should write reply headers and data to the http.ResponseWriter and
+// then return.  Return signals that the request is finished and that the HTTP server
+// can move on to the next request on the connection.  Additionally, information can
+// be shared using context.Context within a given request.
+//
+// ServerHTTP should write reply headers and data to the http.ResponseWriter and
+// then return.  Return signals that the request is finished and that the HTTP server
+// can move on to the next request on the connection.
+type Handler interface {
+	ServeHTTPC(c context.Context, w http.ResponseWriter, r *http.Request)
+	ServeHTTP(w http.ResponseWriter, r *http.Request)
+}
 
-func NewChain(constructors ...Constructor) Chain {
-	c := Chain{}
-	c = append(c, constructors...)
+// A Constructor for a piece of middleware.
+type Constructor func(next Handler) Handler
 
+// Chain acts as a list of Hander constructors.  It is effectively immutable once
+// created; as it will always hold the same set of constructors in the same order.
+type Chain struct {
+	constructors []Constructor
+}
+
+// NewChain creates a new Chain memorizing the given list of middlware Constructors
+func NewChain(constructors ...Constructor) (c Chain) {
+	c.constructors = append(c.constructors, constructors...)
 	return c
 }
 
-func (c Chain) Then(h HandlerFunc) HandlerFunc {
+// Then chains the middleware and returns the final Handler.
+//
+// A chain can be safely reused calling Then() serveral times.  Note that constructors
+// are called on every call to Then() and thus serveral instances of the same middleware
+// will be created what a Chain is reused in this way.
+//
+// Nil with result in a panic.
+func (c Chain) Then(h Handler) Handler {
 	if h == nil {
 		panic("expected HandlerFunc")
 	}
 
 	var final = h
 
-	for i := len(c) - 1; i >= 0; i-- {
-		final = c[i](final)
+	for i := len(c.constructors) - 1; i >= 0; i-- {
+		final = c.constructors[i](final)
 	}
 
 	return final
 }
 
+// ThenFunc works identically to Then but takes a HandlerFunc instead of a Handler.
+func (c Chain) ThenFunc(fn HandlerFunc) Handler {
+	return c.Then(HandlerFunc(fn))
+}
+
+// Append extends a Chain adding the specified constructors as the last ones in the
+// request flow.
+//
+// Append returns a new chain leaving the original untoched.
 func (c Chain) Append(constructors ...Constructor) Chain {
-	newCons := make([]Constructor, len(c)+len(constructors))
-	copy(newCons, c)
-	copy(newCons[len(c):], constructors)
+	newCons := make([]Constructor, len(c.constructors)+len(constructors))
+	copy(newCons, c.constructors)
+	copy(newCons[len(c.constructors):], constructors)
 
 	newChain := NewChain(newCons...)
 	return newChain
 }
 
+// Extend extends a Chain by adding the specified Chain as the last on in the
+// request flow.
+//
+// Extend returns a new Chain leaving the original untouched.
 func (c Chain) Extend(chain Chain) Chain {
-	return c.Append(chain...)
+	return c.Append(chain.constructors...)
 }
--- synapse_test.go
+++ synapse_test.go
@@ -32,59 +32,80 @@ func TestHandlerFunc(t *testing.T) {
 		}
 	})
 
+	fn.ServeHTTPC(context.Background(), res, req)
+}
+
+func TestHandlerFunc_SatisfiesHttpHandler(t *testing.T) {
+	res := &httptest.ResponseRecorder{}
+	req := &http.Request{}
+
+	fn := HandlerFunc(func(c context.Context, w http.ResponseWriter, r *http.Request) {
+		if want, have := context.Background(), c; want != have {
+			t.Error("unexpected context:", have)
+		}
+		if want, have := res, w; want != have {
+			t.Error("unexpected resposne:", have)
+		}
+		if want, have := req, r; want != have {
+			t.Error("unexpected request:", have)
+		}
+	})
+
 	fn.ServeHTTP(res, req)
 }
 
 func testLogMiddleware(logs io.Writer) Constructor {
-	return func(next HandlerFunc) HandlerFunc {
-		return func(c context.Context, w http.ResponseWriter, r *http.Request) {
+	return func(next Handler) Handler {
+		return HandlerFunc(func(c context.Context, w http.ResponseWriter, r *http.Request) {
 			logger := log.New(logs, "", 0)
 
 			defer func(begin time.Time) {
 				logger.Printf("method=%s route=%s took=%s", r.Method, r.RequestURI, time.Now().Sub(begin))
 			}(time.Now())
 
-			next(c, w, r)
-		}
+			next.ServeHTTPC(c, w, r)
+		})
 	}
 }
 
 func testUserMiddleware() Constructor {
-	return func(next HandlerFunc) HandlerFunc {
-		return func(c context.Context, w http.ResponseWriter, r *http.Request) {
-			next(context.WithValue(c, "user", "foobarbaz"), w, r)
-		}
+	return func(next Handler) Handler {
+		return HandlerFunc(func(c context.Context, w http.ResponseWriter, r *http.Request) {
+			next.ServeHTTPC(context.WithValue(c, "user", "foobarbaz"), w, r)
+		})
 	}
 }
 
 func testContentTypeMiddleware() Constructor {
-	return func(next HandlerFunc) HandlerFunc {
-		return func(c context.Context, w http.ResponseWriter, r *http.Request) {
+	return func(next Handler) Handler {
+		return HandlerFunc(func(c context.Context, w http.ResponseWriter, r *http.Request) {
 			w.Header().Set("Content-Type", "text/plain")
-			next(c, w, r)
-		}
+			next.ServeHTTPC(c, w, r)
+		})
 	}
 }
 
-func testHelloHandler(c context.Context, w http.ResponseWriter, _ *http.Request) {
-	fmt.Fprintf(w, "Hello, %s!\n", c.Value("user"))
+func testHelloHandler() HandlerFunc {
+	return HandlerFunc(func(c context.Context, w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %s!\n", c.Value("user"))
+	})
 }
 
 func TestNewChain(t *testing.T) {
-	c1 := func(HandlerFunc) HandlerFunc {
+	c1 := func(Handler) Handler {
 		return nil
 	}
-	c2 := func(HandlerFunc) HandlerFunc {
+	c2 := func(Handler) Handler {
 		return nil
 	}
 
 	slice := []Constructor{c1, c2}
 	chain := NewChain(slice...)
 
-	if want, have := true, funcsEqual(chain[0], c1); want != have {
+	if want, have := true, funcsEqual(chain.constructors[0], c1); want != have {
 		t.Error("unexpected constructor [0]:", have)
 	}
-	if want, have := true, funcsEqual(chain[1], c2); want != have {
+	if want, have := true, funcsEqual(chain.constructors[1], c2); want != have {
 		t.Error("unexpected constructor [1]:", have)
 	}
 }
@@ -101,7 +122,18 @@ func TestChain_ThenPanicsIfHandlerNil(t *testing.T) {
 	}()
 
 	chain := NewChain().Then(nil)
-	chain.ServeHTTP(nil, nil)
+	chain.ServeHTTPC(nil, nil, nil)
+
+	t.Error("should have paniced")
+}
+
+func TestChain_ThenFuncPanicsIfHandlerNil(t *testing.T) {
+	defer func() {
+		recover()
+	}()
+
+	chain := NewChain().ThenFunc(nil)
+	chain.ServeHTTPC(nil, nil, nil)
 
 	t.Error("should have paniced")
 }
@@ -114,9 +146,24 @@ func TestChain_ThenWorksWithNoMiddleware(t *testing.T) {
 	}()
 
 	chain := NewChain()
-	final := chain.Then(testHelloHandler)
+	final := chain.Then(testHelloHandler())
 
-	if want, have := true, funcsEqual(final, testHelloHandler); want != have {
+	if want, have := true, funcsEqual(final, testHelloHandler()); want != have {
+		t.Error("unexpected final:", have)
+	}
+}
+
+func TestChain_ThenFuncWorksWithNoMiddleware(t *testing.T) {
+	defer func() {
+		if r := recover(); r != nil {
+			t.Fatal("unexpected panic:", r)
+		}
+	}()
+
+	chain := NewChain()
+	final := chain.ThenFunc(testHelloHandler())
+
+	if want, have := true, funcsEqual(final, testHelloHandler()); want != have {
 		t.Error("unexpected final:", have)
 	}
 }
@@ -128,7 +175,7 @@ func TestChain_ThenOrdersHandlerFuncsRight(t *testing.T) {
 		testLogMiddleware(logs),
 		testUserMiddleware(),
 		testContentTypeMiddleware(),
-	).Then(testHelloHandler)
+	).Then(testHelloHandler())
 
 	w := httptest.NewRecorder()
 	r, err := http.NewRequest("GET", "/", nil)
@@ -136,7 +183,7 @@ func TestChain_ThenOrdersHandlerFuncsRight(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	chained.ServeHTTP(w, r)
+	chained.ServeHTTPC(context.Background(), w, r)
 
 	if want, have := "text/plain", w.Header().Get("Content-Type"); want != have {
 		t.Error("unexpected response header [Content-Type]:", have)
@@ -152,14 +199,14 @@ func TestChain_AppendAddsHandlerFuncsCorrectly(t *testing.T) {
 	chain := NewChain(testLogMiddleware(logs))
 	newChain := chain.Append(testUserMiddleware(), testContentTypeMiddleware())
 
-	if want, have := 1, len(chain); want != have {
+	if want, have := 1, len(chain.constructors); want != have {
 		t.Error("unexpected len:", have)
 	}
-	if want, have := 3, len(newChain); want != have {
+	if want, have := 3, len(newChain.constructors); want != have {
 		t.Error("unexpected len:", have)
 	}
 
-	chained := newChain.Then(testHelloHandler)
+	chained := newChain.Then(testHelloHandler())
 
 	w := httptest.NewRecorder()
 	r, err := http.NewRequest("GET", "/", nil)
@@ -167,7 +214,7 @@ func TestChain_AppendAddsHandlerFuncsCorrectly(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	chained.ServeHTTP(w, r)
+	chained.ServeHTTPC(context.Background(), w, r)
 
 	if want, have := "text/plain", w.Header().Get("Content-Type"); want != have {
 		t.Error("unexpected response header [Content-Type]:", have)
@@ -181,7 +228,7 @@ func TestChain_AppendRespectsImmutability(t *testing.T) {
 	chain := NewChain(testUserMiddleware())
 	newChain := chain.Append(testUserMiddleware())
 
-	if one, two := &chain[0], &newChain[0]; one == two {
+	if one, two := &chain.constructors[0], &newChain.constructors[0]; one == two {
 		t.Error("unexpected equality")
 	}
 }
@@ -193,17 +240,17 @@ func TestChain_ExtendAddsHandlerFuncsCorrectly(t *testing.T) {
 	chain2 := NewChain(testUserMiddleware(), testContentTypeMiddleware())
 	newChain := chain1.Extend(chain2)
 
-	if want, have := 1, len(chain1); want != have {
+	if want, have := 1, len(chain1.constructors); want != have {
 		t.Error("unexpected len:", have)
 	}
-	if want, have := 2, len(chain2); want != have {
+	if want, have := 2, len(chain2.constructors); want != have {
 		t.Error("unexpected len:", have)
 	}
-	if want, have := 3, len(newChain); want != have {
+	if want, have := 3, len(newChain.constructors); want != have {
 		t.Error("unexpected len:", have)
 	}
 
-	chained := newChain.Then(testHelloHandler)
+	chained := newChain.Then(testHelloHandler())
 
 	w := httptest.NewRecorder()
 	r, err := http.NewRequest("GET", "/", nil)
@@ -211,7 +258,7 @@ func TestChain_ExtendAddsHandlerFuncsCorrectly(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	chained.ServeHTTP(w, r)
+	chained.ServeHTTPC(context.Background(), w, r)
 
 	if want, have := "text/plain", w.Header().Get("Content-Type"); want != have {
 		t.Error("unexpected response header [Content-Type]:", have)
@@ -225,9 +272,9 @@ func TestExtendRespectsImmutability(t *testing.T) {
 	chain := NewChain(testUserMiddleware())
 	newChain := chain.Extend(NewChain(testUserMiddleware()))
 
-	if one, two := &chain[0], &newChain[0]; one == two {
+	if one, two := &chain.constructors[0], &newChain.constructors[0]; one == two {
 		t.Error("unexpected equality")
 	}
 
-	assert.NotEqual(t, &chain[0], &newChain[0])
+	assert.NotEqual(t, &chain.constructors[0], &newChain.constructors[0])
 }
